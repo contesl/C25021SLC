398057e8345eb311737dab9e104d606f77b8dd89
sconte <contesl@gmail.com>
2025-04-13 10:33:14 -0300

    cambio en el nombre del directorio de css

--- 2000s-style/src/main.jsx
+++ 2000s-style/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Importa desde 'react-dom/client'
 import App from './App';
-import './css/styles.css';
+import './styles/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Usa createRoot
 root.render(
