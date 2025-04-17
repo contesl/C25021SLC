7aa8ef4ca9deb519acfcd3bdc899b77354909db5
sconte <contesl@gmail.com>
2025-04-17 05:26:21 -0300

    github

--- 2000s-style/vite.config.js
+++ 2000s-style/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: "https://github.com/contesl/C25021SLC",
+  base: "https://github.com/contesl/C25021SLC.git",
   plugins: [react()],
 })
